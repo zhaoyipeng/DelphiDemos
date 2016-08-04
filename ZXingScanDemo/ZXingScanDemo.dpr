e1dc3ed552112a6e3ea6178c6aa932ff2e97d5e1
zhaoyipeng <zhaoyipeng@hotmail.com>
2016-08-05 00:06:32 +0800

    add ZXingScanDemo

---
+++ ZXingScanDemo/ZXingScanDemo.dpr
@@ -0,0 +1,14 @@
+program ZXingScanDemo;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  main in 'main.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
